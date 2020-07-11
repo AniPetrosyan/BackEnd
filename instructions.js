20d30925f6934d594cabb01bada47b7fd95399d9
AniPetrosyan <ani.petrosyan.y@tumo.org>
2020-07-11 18:45:12 +0400

    AddingInstructions

---
+++ instructions.js
@@ -0,0 +1,46 @@
+/*  
+
+*Sign up:    https://radiant-tundra-50455.herokuapp.com/user/auth/signup
+             method: POST
+             requirements: username, email, password
+
+
+
+*Sign in:    https://radiant-tundra-50455.herokuapp.com/user/auth/signin 
+             method: POST
+             requirements: email, password 
+             
+
+             
+*Profile:    https://radiant-tundra-50455.herokuapp.com/user/auth/profile 
+             method: GET
+           
+             
+*All Posts:  https://radiant-tundra-50455.herokuapp.com/posts
+             method: GET
+
+
+*Post by Id:  https://radiant-tundra-50455.herokuapp.com/post/:id
+              method: GET      
+              
+
+
+*Add Post:  https://radiant-tundra-50455.herokuapp.com/posts/add
+            method: POST
+            requirements: username, category, imgUrl, describtion
+
+
+
+
+*Delete Post:  https://radiant-tundra-50455.herokuapp.com/posts/del/:id
+               method: POST
+
+
+*User by UserId:  https://radiant-tundra-50455.herokuapp.com/posts/user/:userId
+                  method: POST       
+                  
+                  
+*Update:  https://radiant-tundra-50455.herokuapp.com/posts/update/:id
+                  method: PATCH                     
+                       
+*/
